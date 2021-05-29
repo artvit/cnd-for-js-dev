ff028e62af21d570940ccb7a3f5621766efc9483
Artem Vitkovsky <artvitk@gmail.com>
2021-05-29 17:46:26 +0300

    feat: add ui code to the repository

--- build_ui.sh
+++ build_ui.sh
@@ -1,29 +1,7 @@
 #!/bin/sh
-ui_environment="export const environment = {
-  production: true,
-  isAuthorizationEnabled: false,
-  apiUrls: {
-    auth: '',
-    quote: '/api',
-    share: '',
-  },
-};"
-current_dir=$(pwd)
+project_root_dir=$(pwd)
 rm -rf static
-cd ..
-if [ -d "./ngQuote" ]
-then
-  echo "UI repository is found"
-else
-  git clone git@github.com:boale/ngQuote.git
-  git checkout develop
-  npm ci
-fi
-cd "ngQuote" || exit
+cd "client" || exit 1
 rm -rf dist
-echo "$ui_environment" > src/environments/environment.prod.ts
 npm run build -- --prod
-mv dist "$current_dir/t_dist"
-cd "$current_dir" || exit
-mv t_dist static
-
+mv dist "$project_root_dir/static"
